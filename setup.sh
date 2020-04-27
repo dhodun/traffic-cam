b4ed66d95dc963c7d701e646740fb836188c7ed7
Drew Hodun <dhodun@google.com>
2020-04-27 10:35:06 -0400

    installer updates

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libhdf5-103
 sudo apt-get -y install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 sudo apt-get -y install libatlas-base-dev
 sudo apt-get -y install libjasper-dev
-sudo apt-get upgrade
+sudo apt-get -y upgrade
