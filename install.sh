d38169acf6d7a824029407cf3c25c30724166ae8
Drew Hodun <dhodun@google.com>
2020-04-26 20:54:21 -0400

    installer updates

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ./traffic_cam.service /lib/systemd/system/traffic_cam.service
+sudo cp ./traffic_cam.service /lib/systemd/system/traffic_cam.service
 sudo chmod 644 /lib/systemd/system/traffic_cam.service
 sudo systemctl daemon-reload
 sudo systemctl enable traffic_cam.service
