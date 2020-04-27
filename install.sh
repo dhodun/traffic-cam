fb4ff0da49f95da3958c78e3a7eba9a7013bb02f
Drew Hodun <dhodun@google.com>
2020-04-26 23:14:31 -0400

    installer updates

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: seems to run as root?
 sudo cp ./traffic-cam.service /lib/systemd/system/traffic-cam.service
 sudo chmod 644 /lib/systemd/system/traffic-cam.service
 sudo systemctl daemon-reload
