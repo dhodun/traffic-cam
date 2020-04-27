89e122321a7f9ad362ab5d19d496facaedb7eded
Drew Hodun <dhodun@google.com>
2020-04-27 10:12:26 -0400

    installer updates

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # TODO: seems to run as root?
+# TODO: Maybe change to local so you can see the video that it's capturing?
 sudo cp ./traffic-cam.service /lib/systemd/system/traffic-cam.service
 sudo chmod 644 /lib/systemd/system/traffic-cam.service
 sudo systemctl daemon-reload
--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ pip3 install -r raspberry_pi_requirements.txt
 
 export GOOGLE_APPLICATION_CREDENTIALS='/home/pi/service_account.json'
 
-python3 webcam_to_pubsub.py
+python3 webcam_to_pubsub.py > /home/pi/sample_startup.log 2>&1
